a51160926296433e8000045063041481058cdac4
Kelby Gassman <kelby.gassman@gmail.com>
2023-08-31 12:36:28 +0000

    Style and page updates

---
+++ app/components/footer.tsx
@@ -0,0 +1,18 @@
+const Footer = () => (
+  <footer>
+    <ul className="flex gap-x-4 helvetica">
+      <li>© Kelby Gassman {new Date().getFullYear()}</li>
+      <li>
+        <a href="#">LinkedIn</a>
+      </li>
+      <li>
+        <a href="#">kelby.gassman@gmail.com</a>
+      </li>
+      <li>
+        <a href="#">Resume</a>
+      </li>
+    </ul>
+  </footer>
+);
+
+export default Footer;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Image from 'next/image'
-
-export default function Home() {
+export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-start justify-start p-24">
-     <h1>Kelby Gassman</h1>
-     <h2 className='heading-hero'>Highly Technical UX Designer</h2>
-     <p>More to come... very soon.</p>
-    </main>
-  )
+    <div className="flex min-h-screen flex-col items-start justify-start p-24">
+      <h1>Kelby Gassman</h1>
+      <h2 className="heading-hero">Highly Technical UX Designer</h2>
+      <p>More to come... very soon.</p>
+    </div>
+  );
 }
